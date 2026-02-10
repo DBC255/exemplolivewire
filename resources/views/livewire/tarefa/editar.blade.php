6c1fd5b57a9e742c39250d91289aaf84a1242da4
DBC255 <DBC255>
2026-02-10 15:35:50 -0300

    vulgnares sreo

---
+++ resources/views/livewire/tarefa/editar.blade.php
@@ -0,0 +1,28 @@
+<div class="mt-5">
+    <div class="card">
+        <h5 class="card-header">Cadastro de Tarefas</h5>
+        <div class="card-body">
+            <form wire:submit.prevent="update">
+                <div class="mb-3">
+                    <label for="nome" class="form-label">Nome</label>
+                    <input type="text" class="form-control" wire:model="nome" name="nome" id="nome" placeholder="Ex: estudar">
+                </div>
+
+                <div class="mb-3">
+                    <label for="data_hora" class="form-label">Data_hora</label>
+                    <input type="datetime-local" class="form-control" name="data_hora" id="data_hora" wire:model="data_hora">
+                </div>
+
+                <div class="mb-3">
+                    <label for="descricao" class="form-label">Descricao</label>
+                    <textarea class="form-control" name="descricao" id="descricao" rows="4" wire:model="descricao"></textarea>
+                </div>
+
+                <div class="mb-3">
+                    <button type="submit" class="btn btn-success">Salvar</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
